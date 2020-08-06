49dfc799f44db09a8fc78757422854b8e87109e2
alihanakcm <alihanakcm@gmail.com>
2020-08-06 21:22:08 +0300

    feat:HttpClientModule added

--- onos-web-ui/src/app/app.module.ts
+++ onos-web-ui/src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SectionComponent } from './section/section.component';
-import { MainComponent } from './main/main.component';
+import { DeviceListComponent } from './device-list/device-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     SectionComponent,
-    MainComponent
+    DeviceListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

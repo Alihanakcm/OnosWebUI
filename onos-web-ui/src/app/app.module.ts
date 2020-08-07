aa15f36b95cc352a17077427cbe16aa6b578c4f3
alihanakcm <alihanakcm@gmail.com>
2020-08-08 00:10:25 +0300

    fix: Modules fixed

--- onos-web-ui/src/app/app.module.ts
+++ onos-web-ui/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { SectionComponent } from './section/section.component';
-import { DeviceListComponent } from './device-list/device-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SectionComponent } from './components/section/section.component';
+import { DeviceListComponent } from './components/device-list/device-list.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { PortsComponent } from './components/ports/ports.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { DeviceListComponent } from './device-list/device-list.component';
     NavigationComponent,
     SectionComponent,
     DeviceListComponent,
+    ModalComponent,
+    PortsComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],

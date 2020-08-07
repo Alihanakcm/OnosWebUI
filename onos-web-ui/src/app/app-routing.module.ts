a74373dc30464e9a7d60a730016996012bd7a9e4
alihanakcm <alihanakcm@gmail.com>
2020-08-08 00:09:33 +0300

    feat:Routing added

--- onos-web-ui/src/app/app-routing.module.ts
+++ onos-web-ui/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeviceListComponent } from './components/device-list/device-list.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { PortsComponent } from './components/ports/ports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'devices', pathMatch: 'full' },
+  { path: 'devices', component: DeviceListComponent },
+  { path: 'devices/ports', component: PortsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

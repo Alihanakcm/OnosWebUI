3dede8025c0d03324ca6c72f34c0273c1f95f599
alihanakcm <alihanakcm@gmail.com>
2020-08-13 15:34:42 +0300

    feat: Flow statistic component added

---
+++ onos-web-ui/src/app/components/flow-statistics/flow-statistics.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { StatisticsService } from '../../services/statistic-service/statistics.service';
+import { Table } from 'src/app/entities/table';
+import { Device } from 'src/app/entities/device';
+import { Link } from 'src/app/entities/link';
+import { DeviceService } from 'src/app/services/device-service/device.service';
+
+@Component({
+  selector: 'app-flow-statistics',
+  templateUrl: './flow-statistics.component.html',
+  styleUrls: ['./flow-statistics.component.css'],
+  providers: [StatisticsService, DeviceService],
+})
+export class FlowStatisticsComponent implements OnInit {
+  constructor(
+    private statisticsService: StatisticsService,
+    private deviceService: DeviceService
+  ) {}
+  flowTableStatistics: Table[];
+  devices: Device[];
+  flowsLinks: Link[];
+  getFlowsTableStatisticsByDeviceId(deviceId: string) {
+    this.statisticsService
+      .getFlowsTableStatisticsByDeviceId(deviceId)
+      .subscribe((data) => {
+        this.flowTableStatistics = data;
+      });
+  }
+  getFlowsTableStatistics() {
+    this.statisticsService.getFlowsTableStatistics().subscribe((data) => {
+      this.flowTableStatistics = data;
+    });
+  }
+  ngOnInit(): void {
+    this.deviceService.getDevices().subscribe((data) => {
+      this.devices = data;
+    });
+    this.statisticsService.getFlowsLinkStatistics().subscribe((data) => {
+      this.flowsLinks = data;
+    });
+  }
+}

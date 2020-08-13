8eaaacde7dda7ee98328f0cde5244c0dd07cbc6f
alihanakcm <alihanakcm@gmail.com>
2020-08-13 15:35:04 +0300

    feat: device statistic component added

---
+++ onos-web-ui/src/app/components/device-statistics/device-statistics.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { StatisticsService } from '../../services/statistic-service/statistics.service';
+import { PortStatistic } from 'src/app/entities/portStatistic';
+import { DeviceService } from 'src/app/services/device-service/device.service';
+import { Device } from 'src/app/entities/device';
+
+@Component({
+  selector: 'app-device-statistics',
+  templateUrl: './device-statistics.component.html',
+  styleUrls: ['./device-statistics.component.css'],
+  providers: [StatisticsService, DeviceService],
+})
+export class DeviceStatisticsComponent implements OnInit {
+  constructor(
+    private statisticsService: StatisticsService,
+    private deviceService: DeviceService
+  ) {}
+  devicePortStatistics: PortStatistic[];
+  devices: Device[];
+  getDeviceStatistics(deviceId: string): void {
+    this.statisticsService.getDeviceStatistic(deviceId).subscribe((data) => {
+      this.devicePortStatistics = data;
+    });
+  }
+  getDeviceDeltaStatistics(deviceId: string): void {
+    this.statisticsService
+      .getDeviceDeltaStatistic(deviceId)
+      .subscribe((data) => {
+        console.log(data);
+
+        this.devicePortStatistics = data;
+      });
+  }
+
+  ngOnInit(): void {
+    this.deviceService.getDevices().subscribe((data) => {
+      this.devices = data;
+    });
+  }
+}

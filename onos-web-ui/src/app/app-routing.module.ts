9ec72769fc461d7fc2e0a60c602d4b2526fc37fe
alihanakcm <alihanakcm@gmail.com>
2020-08-18 12:59:15 +0300

    feat: application-install component routing added

--- onos-web-ui/src/app/app-routing.module.ts
+++ onos-web-ui/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DeviceListComponent } from './components/device-list/device-list.component';
-import { ModalComponent } from './components/modal/modal.component';
 import { PortsComponent } from './components/ports/ports.component';
 import { FlowsComponent } from './components/flows/flows.component';
 import { CreateFlowComponent } from './components/create-flow/create-flow.component';
 import { DeviceStatisticsComponent } from './components/device-statistics/device-statistics.component';
 import { FlowStatisticsComponent } from './components/flow-statistics/flow-statistics.component';
+import { ApplicationInstallComponent } from './components/application-install/application-install.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'devices', pathMatch: 'full' },
@@ -16,6 +16,7 @@ const routes: Routes = [
   { path: 'flows/create', component: CreateFlowComponent },
   { path: 'statistics/device', component: DeviceStatisticsComponent },
   { path: 'statistics/flows', component: FlowStatisticsComponent },
+  { path: 'applications/install', component: ApplicationInstallComponent },
 ];
 
 @NgModule({
